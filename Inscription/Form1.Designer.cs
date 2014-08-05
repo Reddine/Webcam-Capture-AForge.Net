8eb14297fd736b48d846083a93ba8e1a3ca9e431
Kheir Eddine FARFAR <kfarfar@gmail.com>
2014-08-05 20:03:34 +0100

    Initial Commit

---
+++ Inscription/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Inscription
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.devicesCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.labelSaved = new System.Windows.Forms.Label();
+            this.takePictureBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // devicesCombo
+            // 
+            this.devicesCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.devicesCombo.FormattingEnabled = true;
+            this.devicesCombo.Location = new System.Drawing.Point(12, 31);
+            this.devicesCombo.Name = "devicesCombo";
+            this.devicesCombo.Size = new System.Drawing.Size(249, 21);
+            this.devicesCombo.TabIndex = 0;
+            this.devicesCombo.SelectedIndexChanged += new System.EventHandler(this.devicesCombo_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose Source :";
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.Location = new System.Drawing.Point(12, 59);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(249, 215);
+            this.videoSourcePlayer.TabIndex = 2;
+            this.videoSourcePlayer.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer.VideoSource = null;
+            // 
+            // labelSaved
+            // 
+            this.labelSaved.AutoSize = true;
+            this.labelSaved.Location = new System.Drawing.Point(9, 295);
+            this.labelSaved.Name = "labelSaved";
+            this.labelSaved.Size = new System.Drawing.Size(89, 13);
+            this.labelSaved.TabIndex = 3;
+            this.labelSaved.Text = "Capture Saved : ";
+            // 
+            // takePictureBtn
+            // 
+            this.takePictureBtn.Location = new System.Drawing.Point(15, 347);
+            this.takePictureBtn.Name = "takePictureBtn";
+            this.takePictureBtn.Size = new System.Drawing.Size(246, 23);
+            this.takePictureBtn.TabIndex = 4;
+            this.takePictureBtn.Text = "Take a #Selfie :)";
+            this.takePictureBtn.UseVisualStyleBackColor = true;
+            this.takePictureBtn.Click += new System.EventHandler(this.takePictureBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 396);
+            this.Controls.Add(this.takePictureBtn);
+            this.Controls.Add(this.labelSaved);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.devicesCombo);
+            this.Name = "Form1";
+            this.Text = "Capture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox devicesCombo;
+        private System.Windows.Forms.Label label1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.Label labelSaved;
+        private System.Windows.Forms.Button takePictureBtn;
+    }
+}
+
---
+++ Inscription/Form1.cs
@@ -0,0 +1,91 @@
+﻿/**
+ * @Author: Kheir Eddine FARFAR
+ * @Author Github: https://github.com/Reddine
+ * @Description: Capture Images from WebCam
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video.DirectShow;
+using System.Drawing.Imaging;
+using System.Net;
+using System.IO;
+
+namespace Inscription
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection videoDevices;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // show device list
+            try
+            {
+                // enumerate video devices
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                // add all devices to combo
+                foreach (FilterInfo device in videoDevices)
+                {
+                    devicesCombo.Items.Add(device.Name);
+                }
+            }
+            catch (ApplicationException)
+            {
+                devicesCombo.Items.Add("No local capture devices");
+                devicesCombo.Enabled = false;
+                takePictureBtn.Enabled = false;
+            }
+
+            devicesCombo.SelectedIndex = 0;
+
+            VideoCaptureDevice videoCaptureSource = new VideoCaptureDevice(videoDevices[devicesCombo.SelectedIndex].MonikerString);
+            videoSourcePlayer.VideoSource = videoCaptureSource;
+            videoSourcePlayer.Start();
+        }
+
+        private void takePictureBtn_Click(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;              // Use current time
+	        string format = "MMM ddd d HH mm yyyy";    // Use this format
+            String strFilename = "Capture-" + time.ToString(format) + ".jpg";
+            if (videoSourcePlayer.IsRunning)
+            {
+                Bitmap picture = videoSourcePlayer.GetCurrentVideoFrame();
+                picture.Save(strFilename, ImageFormat.Jpeg);
+                labelSaved.Text = "Capture Saved : " + strFilename;                
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+            videoDevices = null;
+            videoDevices = null;
+        }
+
+        private void devicesCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+            VideoCaptureDevice videoCaptureSource = new VideoCaptureDevice(videoDevices[devicesCombo.SelectedIndex].MonikerString);
+            videoSourcePlayer.VideoSource = videoCaptureSource;
+            videoSourcePlayer.Start();
+        }
+    }
+}
